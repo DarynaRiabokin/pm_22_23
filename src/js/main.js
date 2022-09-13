ea2690571c51f2e94100768b29a765ce6d74dfc5
Daryna Riabokin <darynariabokin@gmail.com>
2022-09-13 21:44:25 +0300

    added content

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 (() => {
     const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
     const collapseButton = document.getElementById('collapse-menu');
 
     const toggleSidebar = () => {
         if (sidebar.classList.contains('closed')) {
             sidebar.classList.remove('closed');
+            content.classList.remove('col-10');
+            content.classList.add('col-9');
         } else {
             sidebar.classList.add('closed');
+            content.classList.remove('col-9');
+            content.classList.add('col-10');
         }
     }
 
